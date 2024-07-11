a2090843995e44b6eef9f948b3a26da3bce23dc7
SeptianAdhityaN <septiannugroho73@gmail.com>
2024-07-11 23:35:12 +0700

    First Add

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#002662',
+        'secondary': '#36ABA5',
+        'third': '#C5E0F7',
+      },
+    },
+  },
+  plugins: [],
+}
+
